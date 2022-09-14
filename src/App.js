edb17828d9207870edfe1c2a4f2d92efbafef87c
Tomic, Vladimir <Vladimir.Tomic@zuehlke.com>
2022-09-14 14:37:15 +0200

    Add a logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Game from "./components/game/Game";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Game />
     </div>
   );
---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+import React from "react";
+
+export default function Header() {
+  return <div className="header"></div>;
+}
